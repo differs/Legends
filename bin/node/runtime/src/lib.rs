92bbab9b87476e3c325998625dac82474b389790
differs <dareh266@gmail.com>
2021-03-12 15:15:55 +0800

    none

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -105,7 +105,7 @@ pub fn wasm_binary_unwrap() -> &'static [u8] {
 
 /// Runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("legends"),
+	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("legends-node"),
 	authoring_version: 10,
 	// Per convention: if the runtime behavior changes, increment spec_version
